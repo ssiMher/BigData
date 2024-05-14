e990483e1879aed51085db821cf20c61ba11122a
ssiMher <1051407101@qq.com>
2024-05-14 00:38:49 -0700

    add comment

--- src/main/java/Invert01.java
+++ src/main/java/Invert01.java
@@ -16,84 +16,83 @@ import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.StringTokenizer;
-
 public class Invert01 {
     public static class InvertedIndexMapper extends Mapper<LongWritable, Text, Text, Text>
     {
         Text res = new Text();
         @Override
-    protected void map(LongWritable key, Text value, Context context)
-            throws IOException, InterruptedException
-// default RecordReader: LineRecordReader; key: line offset; value: line string
-    {
-        FileSplit fileSplit = (FileSplit)context.getInputSplit();
-        String fileName = fileSplit.getPath().getName();
-        Text word = new Text();
-        //Text fileName_lineOffset = new Text(fileName+"#"+key.toString());
-        //StringTokenizer itr = new StringTokenizer(value.toString());
-        StringTokenizer itr = new StringTokenizer(value.toString().toLowerCase().replaceAll("[^0-9a-zA-Z ]"," "));
-        Hashtable<Text, IntWritable> map = new Hashtable<Text, IntWritable>();
-        //StringTokenizer itr = new StringTokenizer(value.toString());
-        for(; itr.hasMoreTokens(); )
+        protected void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException
         {
-            word.set(itr.nextToken());
-            if(map.containsKey(word)){
-                map.put(word, new IntWritable(map.get(word).get() + 1));
+            FileSplit fileSplit = (FileSplit)context.getInputSplit();
+            String fileName = fileSplit.getPath().getName();
+            Text word = new Text();
+            //获取当前正在处理的文件名，并为存储 Mapper 输出的单词或键值对的键部分创建一个 Text 对象
+            StringTokenizer itr = new StringTokenizer(value.toString().toLowerCase().replaceAll("[^0-9a-zA-Z ]"," "));
+            Hashtable<Text, IntWritable> map = new Hashtable<Text, IntWritable>();
+            /*将输入的文本数据转换为小写形式，并将非字母数字字符替换为空格，然后逐个处理文本中的单词，将每个单词作为键，
+            其出现次数作为值，存储在一个 Hashtable 对象中。*/
+            for(; itr.hasMoreTokens(); )
+            {
+                word.set(itr.nextToken());
+                if(map.containsKey(word)){
+                    map.put(word, new IntWritable(map.get(word).get() + 1));
+                    //用于检查 Hashtable 对象 map 中是否已经包含了当前单词。如果 map 中已经包含了当前单词，则将该单词的出现次数加一
+                }
+                else{
+                    map.put(word, new IntWritable(1));
+                    //如果 map 中没有包含当前单词，则将当前单词添加到 map 中，并将其出现次数设置为1
+                }
             }
-            else{
-                map.put(word, new IntWritable(1));
+            for (Text map_2 : map.keySet()){
+                res.set(fileName + "," + map.get(map_2).toString());
+                context.write(map_2, res);
             }
+            //将每个单词及其在文件中出现的次数写入到 Mapper 的输出中
         }
-
-        for (Text map_2 : map.keySet()){
-            res.set(fileName + "," + map.get(map_2).toString());
-            context.write(map_2, res);
-        }
-    }
     }
     public static class InvertedIndexReducer extends Reducer<Text, Text, Text, Text>
     {
-        //Text res = new Text();
         @Override
-    protected void reduce(Text key, Iterable<Text> values, Context context)
-            throws IOException, InterruptedException
-    {
-        //Iterator<Text> it = values.iterator();
-
-        Hashtable<String, Integer> map = new Hashtable<String, Integer>();
-
-        //Text word = new Text();
-        int fileCnt = 0, sumCnt = 0;
-        for(Text t: values )
+        protected void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException
         {
-            //Text tmp = it.next();
-            String file = t.toString().split(",")[0];
-            int result = Integer.parseInt(t.toString().split(",")[1]);
-            sumCnt+=result;
-            //word.set(t.toString().split(",")[0]);
-            if(map.containsKey(file)){
-                map.put(file,map.get(file)+ result);
+            Hashtable<String, Integer> map = new Hashtable<String, Integer>();
+            //用于存储单词及其出现次数
+            int fileCnt = 0, sumCnt = 0;//分别用于统计文件数量和单词总数。
+            for(Text t: values )
+            {
+                String file = t.toString().split(",")[0];
+                //当前值Text t转换为字符串，并通过逗号分割，提取出文件名
+                int result = Integer.parseInt(t.toString().split(",")[1]);
+                //从当前值Text t中提取出出现次数，并将其转换为整数类型
+                sumCnt+=result;//将当前单词在所有文件中的出现次数累加到sumCnt中
+                if(map.containsKey(file))//用于检查map中是否已经包含了当前文件名
+                {
+                    map.put(file,map.get(file)+ result);
+                    //第一次遇到该文件名，将其添加到map中，并将对应的值设置为当前值
+                }
+                else{
+                    fileCnt++;//统计文件的数量
+                    map.put(file, result);//更新
+                }
             }
-            else{
-                fileCnt++;
-                map.put(file, result);
+            StringBuilder all = new StringBuilder();
+            //构建一个字符串，以便后续将文件名、单词出现次数等信息添加到其中。
+            double avg = (double)sumCnt/fileCnt;//单词的平均出现次数
+            String str_average = String.format("%.2f",avg);
+            all.append("\t" + str_average + ",");
+            //all.append(str_average + ",");
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                all.append(entry.getKey());//将文件名加入
+                all.append(":");
+                all.append(entry.getValue().toString());//加入出现次数
+                all.append(";");
             }
-        }
-        //Iterator<Text> iterator = map.keySet().iterator();
-        StringBuilder all = new StringBuilder();
-        double avg = (double)sumCnt/fileCnt;
-        String str_average = String.format("%.2f",avg);
-        all.append("\t" + str_average + ",");
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            all.append(entry.getKey());
-            all.append(":");
-            all.append(entry.getValue().toString());
-            all.append(";");
-            //System.out.println("key = " + entry.getKey() + ", value = " + entry.getValue());
-        }
 
-        context.write(key, new Text(all.toString()));
-    } //最终输出键值对示例：("fish", “doc1#0; doc1#8;doc2#0;doc2#8 ")
+            context.write(key, new Text(all.toString()));
+            //使用Reducer的上下文对象context将结果写入到输出中。
+        }
     }
     public static void main(String[] args) {
         try {
